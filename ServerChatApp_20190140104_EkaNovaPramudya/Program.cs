f3b2d3e26eceb4373a64d96a388ccfdaae0df7d9
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2022-01-07 22:10:30 +0700

    Add project files.

---
+++ ServerChatApp_20190140104_EkaNovaPramudya/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_2019014014;
+
+namespace ServerChatApp_20190140104_EkaNovaPramudya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = null;
+            try
+            {
+                serviceHost = new ServiceHost(typeof(ServiceCallBack));
+                serviceHost.Open();
+
+                Console.WriteLine("Server is READY....!");
+                Console.ReadLine();
+
+                serviceHost.Close();
+            }
+            catch (Exception e)
+            {
+                serviceHost = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
